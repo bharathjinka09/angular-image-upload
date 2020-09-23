dfabd4db0626aebab4b94546dd53af062f92d710
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-23 18:23:35 +0530

    angular-image-upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  url = '';
+
+  urls = [];
+
+  path: String;
+
+  constructor(private http: HttpClient, private af: AngularFireStorage) {}
+
+  upload(event) {
+    this.path = event.target.files[0];
+  }
+
+  uploadImage() {
+    console.log(this.path);
+
+    this.af.upload('/files' + Math.random() + this.path, this.path);
+    alert('File uploaded successfully!')
+  }
+
+  selectFiles(event) {
+    if (event.target.files) {
+      for (var i = 0; i < File.length; i++) {
+        var reader = new FileReader();
+        reader.readAsDataURL(event.target.files[i]);
+        reader.onload = (event: any) => {
+          this.urls.push(event.target.result);
+        };
+      }
+    }
+  }
+
+  selectFile(event) {
+    if (event.target.files) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event: any) => {
+        this.url = event.target.result;
+      };
+    }
+  }
+
   title = 'angular-image-upload';
+  selectedFile: File = null;
+
+  onFileSelected(event) {
+    console.log(event);
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+  onUpload() {
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name);
+    this.http
+      .post('https://upload.uploadcare.com/base/', fd, {
+        reportProgress: true,
+        observe: 'events',
+      })
+      .subscribe((event) => {
+        if (event.type === HttpEventType.UploadProgress) {
+          console.log(
+            'Upload progress' +
+              Math.round((event.loaded / event.total) * 100) +
+              '%'
+          );
+        } else if (event.type === HttpEventType.Response) {
+          console.log(event);
+        }
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    AngularFireStorageModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyBXDR1lVnNnCduj-lfcn5L-CTwMa_DHYao',
+      authDomain: 'angular-file-upload-caef1.firebaseapp.com',
+      databaseURL: 'https://angular-file-upload-caef1.firebaseio.com',
+      projectId: 'angular-file-upload-caef1',
+      storageBucket: 'angular-file-upload-caef1.appspot.com',
+      messagingSenderId: '668633930035',
+      appId: '1:668633930035:web:cb3cf3b43229a1277a4646',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
